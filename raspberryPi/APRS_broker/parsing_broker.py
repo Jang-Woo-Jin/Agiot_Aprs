fcc443353792fa78e33ed9d85675379d399b16fb
woojin <ehdwlsdnwls@naver.com>
2019-12-11 16:11:31 -0500

    Fix usb port error

--- raspberryPi/APRS_broker/parsing_broker.py
+++ raspberryPi/APRS_broker/parsing_broker.py
@@ -5,32 +5,44 @@ import serial
 end = False
 
 def func(second = 1.0):
-        global end
-        if end:
-            return
-        f = open("/home/pi/output_mqtt.txt",'r')
-        print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        s = f.read()
-        print(s)
-        f.close()
-        f = open("/home/pi/output_mqtt.txt", 'w')
-        f.close()
+    global end
+    if end:
+        return
+    f = open("/home/pi/output_mqtt.txt",'r')
+    print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    s = f.read()
+    print(s)
+    f.close()
+    f = open("/home/pi/output_mqtt.txt", 'w')
+    f.close()
 
-        lines = s.splitlines()
-        try:
-                datas = lines[-1].split("!")[1]
-                print(datas)
-                ser = serial.Serial('/dev/ttyUSB0', 9600)
-                if int(datas) < 200:
-                        ser.write('1')
-                        print("1")
-                else:
-                        ser.write('0')
-                        print("0")
-        except:
-                print("exceptions")
+    lines = s.splitlines()
+    try:
+        datas = lines[-1].split("!")[1]
+        print(datas)
+        
+        port_num = 0
+        usb_port = '/dev/ttyUSB'
+        port_flag = True
+        while(port_flag):
+            try:
+                ser = serial.Serial(usb_port + port_num, 9600)
+                port_flag = False
+            except:
+                print("port number error : " + str(port_num))
+                port_num = port_num + 1
+                port_num = port_num % 5
 
-        threading.Timer(second, func, [second]).start()
+        if int(datas) < 200:
+            ser.write('1')
+            print("1")
+        else:
+            ser.write('0')
+            print("0")
+    except:
+        print("exceptions")
+
+    threading.Timer(second, func, [second]).start()
 
 def main():
     func(2.0)
