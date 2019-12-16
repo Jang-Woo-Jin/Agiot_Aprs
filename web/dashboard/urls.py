7717a43b9ec7e3f15012c46dbd5e37fa4eb6bc0a
woojin <ehdwlsdnwls@naver.com>
2019-12-16 10:01:05 -0500

    add farm adding function

--- web/dashboard/urls.py
+++ web/dashboard/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.dash_farm, name='dash_farm'),
     path('dashboard/farm/', views.dash_farm, name='dash_farm'),
+    path('dashboard/farm/create', views.farm_create, name='farm_create'),
     path('dashboard/sensor/', views.dash_farm, name='dash_farm'),
 ]
--- web/dashboard/views.py
+++ web/dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from dashboard.models import Farm
 
@@ -9,3 +9,15 @@ def dash_farm(request):
     farms = Farm.objects.filter(user=user.username)
     print(farms)
     return render(request, 'dashboard/dash_farm.html', {'user': user, 'farms': farms})
+
+
+def farm_create(request):
+    if request.method == "POST":
+        farm = Farm()
+        farm.user = request.user
+        farm.name = request.POST["name"]
+        farm.location = request.POST["location"]
+        farm.type = request.POST["type"]
+        farm.save()
+        return redirect('dash_farm')
+    return render(request, 'dashboard/farm_create.html')
